4279603c0c527c82719906fc3f18f723b8c1265d
rbisha50 <rbishal50@gmail.com>
2020-07-17 22:30:02 +0545

    Updates

---
+++ precache-manifest.6eb49ffd5bca37487b615ca931f0875e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493e89cb36ad23631ac546afc924f880",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "869f8090b2c655414161",
+    "url": "/monsters-rolodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869f8090b2c655414161",
+    "url": "/monsters-rolodex/static/js/main.b46db8b7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
